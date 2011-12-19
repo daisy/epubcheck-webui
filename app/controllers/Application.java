e341ee9ea148275b67172fd24569005c0cd2b8f2
Romain Deltour <rdeltour@gmail.com>
2011-12-20 00:46:06 +0100

    Cosmetic edits. Spearated Issue-to-Map conversion into a dedicated function

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,6 +2,7 @@ package controllers;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,32 +10,34 @@ import java.util.Map;
 import org.daisy.validation.epubcheck.EpubcheckBackend;
 import org.daisy.validation.epubcheck.EpubcheckBackend.Issue;
 
+import com.google.common.base.Function;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
 import play.Logger;
 import play.data.FileUpload;
 import play.libs.Codec;
 import play.mvc.Controller;
 
 public class Application extends Controller {
-	public static void index() {
-		render();
-	}
 
 	private static final String RESULTS = "results";
 	private static final String FILENAME = "filename";
 	private static final String FILEERROR = "fileError";
-	
+
+	public static void index() {
+		render();
+	}
+
 	public static void validate(final FileUpload input_file) {
 		List<Map<String, String>> results = null;
 		String filename = null;
-		boolean fileError = false;
-		if (input_file == null) {
-			fileError = true;
-		}
-		else {
+		boolean fileError = input_file == null;
+		if (!fileError) {
 			final String origFileName = input_file.getFileName();
 			filename = new File(origFileName).getName();
-			final File newFile = input_file.asFile(new File(play.Play.tmpDir, Codec
-					.UUID() + ".epub"));
+			final File newFile = input_file.asFile(new File(play.Play.tmpDir,
+					Codec.UUID() + ".epub"));
 			results = runEpubcheck(newFile.getPath());
 			final boolean deleted = newFile.delete();
 			if (!deleted) {
@@ -46,19 +49,20 @@ public class Application extends Controller {
 		renderArgs.put(FILENAME, filename);
 		render();
 	}
-		
+
 	private static List<Map<String, String>> runEpubcheck(final String file) {
-		final List<Issue> issues = EpubcheckBackend.run(file);
-		final List<Map<String, String>> results = new ArrayList<Map<String, String>>();
-		for (final Issue issue : issues) {
-			final Map<String, String> result = new HashMap<String, String>();
-			result.put("type", issue.type);
-			result.put("file", issue.file);
-			result.put("lineNr", Integer.toString(issue.lineNo));
-			result.put("position", Integer.toString(issue.colNo));
-			result.put("message", issue.txt);
-			results.add(result);
-		}
-		return results;
+		return Lists.transform(EpubcheckBackend.run(file), issueToMap);
 	}
+
+	private static Function<Issue, Map<String, String>> issueToMap = new Function<EpubcheckBackend.Issue, Map<String, String>>() {
+		public Map<String, String> apply(Issue issue) {
+			Map map = Maps.newHashMapWithExpectedSize(5);
+			map.put("type", issue.type);
+			map.put("file", issue.file);
+			map.put("lineNr", Integer.toString(issue.lineNo));
+			map.put("position", Integer.toString(issue.colNo));
+			map.put("message", issue.txt);
+			return Collections.unmodifiableMap(map);
+		}
+	};
 }
