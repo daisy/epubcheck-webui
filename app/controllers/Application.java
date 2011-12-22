4c3e4ab3d4cf238220d8b0601b9c8e5bc8e53a7a
Romain Deltour <rdeltour@gmail.com>
2011-12-22 12:55:00 +0100

    Added support for EPUB version detection with the new epubcheck 3.0b4

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,31 +1,32 @@
 package controllers;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.daisy.validation.epubcheck.EpubcheckBackend;
-import org.daisy.validation.epubcheck.EpubcheckBackend.Issue;
-
-import com.google.common.base.Function;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
+import org.daisy.validation.epubcheck.Issue;
 
 import play.Logger;
 import play.data.FileUpload;
 import play.libs.Codec;
 import play.mvc.Controller;
 
+import com.google.common.base.Function;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Collections2;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
 public class Application extends Controller {
 
 	private static final String RESULTS = "results";
+	private static final String VERSION = "version";
 	private static final String FILENAME = "filename";
 	private static final String FILEERROR = "fileError";
 
-	private static final Function<Issue, Map<String, String>> issueToMap = new Function<EpubcheckBackend.Issue, Map<String, String>>() {
+	private static final Function<Issue, Map<String, String>> issueToMap = new Function<Issue, Map<String, String>>() {
 		public Map<String, String> apply(Issue issue) {
 			Map map = Maps.newHashMapWithExpectedSize(5);
 			map.put("type", issue.type);
@@ -36,13 +37,13 @@ public class Application extends Controller {
 			return Collections.unmodifiableMap(map);
 		}
 	};
-	
+
 	public static void index() {
 		render();
 	}
 
 	public static void validate(final FileUpload input_file) {
-		List<Map<String, String>> results = null;
+		Iterable<Map<String, String>> results = null;
 		String filename = null;
 		boolean fileError = input_file == null;
 		if (!fileError) {
@@ -63,7 +64,15 @@ public class Application extends Controller {
 	}
 
 	private static List<Map<String, String>> runEpubcheck(final String file) {
-		return Lists.transform(EpubcheckBackend.run(file), issueToMap);
+		List<Issue> results = EpubcheckBackend.run(file);
+		if (results.get(0).type == "Version") {
+			renderArgs.put(VERSION, results.get(0).txt);
+		}
+		return Collections.unmodifiableList(Lists.newArrayList(Collections2
+				.transform(Collections2.filter(results, new Predicate<Issue>() {
+					public boolean apply(Issue issue) {
+						return issue.type != "Version";
+					}
+				}), issueToMap)));
 	}
-
 }
